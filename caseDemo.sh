44633fa996b9e290355292e5c84bbe432974a265
sheetalc818 <sheetalc818@gmail.com>
2023-06-12 14:41:50 +0530

    ADDED : caseDemo.sh file

---
+++ caseDemo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+isFullTimePresent=1;
+isPartTimePresent=2;
+randomCheck=$((RANDOM%3));
+
+case $randomCheck in
+        1)
+        echo "Engineer is present";
+                  ;;
+        2)
+        echo "Engineer partially present";
+                  ;;
+        *)
+                echo "Engineer is Abscent";
+                ;;
+esac
+
+
+
