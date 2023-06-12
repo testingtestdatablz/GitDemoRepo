7b3775c78b425f4afa7538ab271ea1c6eba6b812
sheetalc818 <sheetalc818@gmail.com>
2023-06-12 15:13:33 +0530

    MODIFIED : Emp Hrs

--- caseDemo.sh
+++ caseDemo.sh
@@ -6,13 +6,16 @@ randomCheck=$((RANDOM%3));
 case $randomCheck in
         1)
         echo "Engineer is present";
-                  ;;
+	      empHrs=8;
+              ;;
         2)
         echo "Engineer partially present";
-                  ;;
+	      empHrs=4;         
+              ;;
         *)
-                echo "Engineer is Abscent";
-                ;;
+        echo "Engineer is Abscent";
+              empHrs=0;
+              ;;
 esac
 
 
